ffecf49ef3a5eb9438cd26a4eeea6aa4641297d7
ThomasL-GD <swegews.yeek@gmail.com>
2020-02-06 16:53:08 +0100

    Final ?

--- Get_lost_by_running.c
+++ Get_lost_by_running.c
@@ -9,6 +9,7 @@ struct Room{
 	char description[255];
 	int difficulty;
 	int connection [12];
+	int bBase;
 	
 };
 typedef struct Room room;
@@ -21,12 +22,13 @@ int main(){
 		printf("\n");
 	}
 	
-	room tRoom[12] = {"entrance", "Simply th entrance room, this is where you come from",1,{0,1,1,0,0,0,0,0,0,0,0,0},"empty","It seems to be a clear and empty room...",1,{1,0,1,0,0,0,0,0,0,0,0,0},"dark","It is a dark and cold room, you better not staying long in there",10,{0,1,0,1,1,1,0,0,0,0,0,0},"strange","It is a strange room where a weird feeling reigns...",5,{0,1,1,0,1,0,0,1,0,0,0,0},"warm","It is a room where you feel very hot, it looks like the global warming was not a fake...",11,{0,0,0,0,0,1,1,0,0,0,0,0},"hardcore","This is a horrible room where you cannot step back...Why did you go in a room with such a name ?!",15,{1,0,0,0,0,0,0,0,0,0,0,0},"magical","It is a safe room that make you feel better about your journey through this strange donjon",4,{0,0,0,0,1,1,0,1,0,0,0,0},"pause","Uh oh... You taught this was a pause room for you, but this is the pause room of all the monsters in the donjon ! RUN !!!",17,{0,0,0,0,0,0,1,0,1,0,0,0},"cold","It is a room where a cold atmosphere reigns, you better leaving quickly before taking cold >_<",9,{0,0,1,0,1,0,0,1,0,1,0,0},"T3leP0rTati0n_#","Despite its strange name, this room is really a teleportation room... But it feels like the rooms where you can go are changing each time you go in this room...",5,{1,0,0,0,0,0,0,0,0,0,1,1},"?EXIT?","Phew, this is finally the exit room, you can go out of the donjon now...oh wait...don't tell me that...no...YOU FORGOT THE KEYS ?",2,{1,0,0,0,0,0,0,0,0,0,0,0},"SATANIC","OH MY GOD RUN AWAY !!! THERE ARE DEMONS EVERYWHERE !!! WHY ARE YOU STUPID AND YOU DECIDED TO GO IN THERE ?!!",19,{0,0,1,0,0,0,0,0,0,0,0,1}};
+	room tRoom[12] = {"entrance", "Simply th entrance room, this is where you come from",1,{0,1,1,0,0,0,0,0,0,0,0,0},0,"empty","It seems to be a clear and empty room...",1,{1,0,1,0,0,0,0,0,0,0,0,0},0,"dark","It is a dark and cold room, you better not staying long in there",10,{0,1,0,1,1,1,0,0,0,0,0,0},0,"strange","It is a strange room where a weird feeling reigns...",5,{0,1,1,0,1,0,0,1,0,0,0,0},0,"warm","It is a room where you feel very hot, it looks like the global warming was not a fake...",11,{0,0,0,0,0,1,1,0,0,0,0,0},0,"hardcore","This is a horrible room where you cannot step back...Why did you go in a room with such a name ?!",15,{1,0,0,0,0,0,0,0,0,0,0,0},0,"magical","It is a safe room that make you feel better about your journey through this strange donjon",4,{0,0,0,0,1,1,0,1,0,0,0,0},0,"pause","Uh oh... You taught this was a pause room for you, but this is the pause room of all the monsters in the donjon ! RUN !!!",17,{0,0,0,0,0,0,1,0,1,0,0,0},0,"cold","It is a room where a cold atmosphere reigns, you better leaving quickly before taking cold >_<",9,{0,0,1,0,1,0,0,1,0,1,0,0},0,"T3leP0rTati0n_#","Despite its strange name, this room is really a teleportation room... But it feels like the rooms where you can go are changing each time you go in this room...",5,{1,0,0,0,0,0,0,0,0,0,1,1},0,"?EXIT?","Phew, this is finally the exit room, you can go out of the donjon now...oh wait...don't tell me that...no...YOU FORGOT THE KEYS ?",2,{1,0,0,0,0,0,0,0,0,0,0,0},0,"SATANIC","OH MY GOD RUN AWAY !!! THERE ARE DEMONS EVERYWHERE !!! WHY ARE YOU STUPID AND YOU DECIDED TO GO IN THERE ?!!",19,{0,0,1,0,0,0,0,0,0,0,0,1},0};
 	int bGame = 0;
 	int nChoice = 0;
 	char  sChoice[100] = "";
 	int nCurrentRoom = 0;
 	int tPossible[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+	int stamina = 100;
 	
 	while(bGame == 0){
 		
@@ -45,6 +47,7 @@ int main(){
 				tPossible[i] = 0;
 			}
 			
+			printf("	current stamina : %d\n",stamina);
 			printf("Where do you want to go ?\n");
 			
 			for(int i = 0; i <12; i++){
@@ -54,6 +57,12 @@ int main(){
 				}
 			}
 			
+			if(tRoom[nCurrentRoom].difficulty > 10 && tRoom[nCurrentRoom].bBase == 0){
+			printf("Put) Put a base in this room\n");
+			}
+			if(tRoom[nCurrentRoom].difficulty < 5){
+			printf("Take) Take a break\n");
+			}
 			printf("Quit) Quit game (please don't T_T)\n");
 			
 			scanf("%99s", sChoice);
@@ -62,14 +71,35 @@ int main(){
 				
 				if(strcmp(sChoice, tRoom[i].name) == 0 && tPossible[i] == 1){
 					printf("Woaw you arrived at the \"%s\" room !\n", tRoom[i].name);
-					nCurrentRoom = i;
 					nChoice = 1;
 					printf("%s\n", tRoom[i].description);
+					if(tRoom[nCurrentRoom].bBase == 1){printf("There is a base on this room\n");}
+					stamina -= tRoom[i].difficulty + tRoom[nCurrentRoom].difficulty;
+					nCurrentRoom = i;
 				}
 				
 			}
 			
+			if(strcmp(sChoice, "Put")==0 && tRoom[nCurrentRoom].difficulty > 10 && tRoom[nCurrentRoom].bBase == 0){
+				nChoice = 1;
+				tRoom[nCurrentRoom].difficulty = tRoom[nCurrentRoom].difficulty/2;
+				tRoom[nCurrentRoom].bBase = 1;
+				printf("You successfully put a base here !\n");
+			}
+			
+			if(strcmp(sChoice, "Take")==0 && tRoom[nCurrentRoom].difficulty < 5){
+				nChoice = 1;
+				stamina += 50;
+				if(stamina > 100){stamina = 100;}
+				printf("You take a break and recover\n");
+			}
+			
 			if(strcmp(sChoice, "Quit")==0){
+				nChoice = 1;
+				bGame = 1;
+			}
+			
+			if(stamina <= 0){
 				nChoice = 1;
 				bGame = 1;
 			}
